78b69a4b45dc73ca824e2d251d18b0d18a08cc2b
SNEYKERZ <alejoarenasdev@gmail.com>
2025-01-23 17:12:10 -0500

    falta que me tome las rutas

--- application/controllers/Camara.php
+++ application/controllers/Camara.php
@@ -6,6 +6,9 @@ class Camara extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
+        $this->load->helper('url');
+        $this->load->helper('form');
+        
     }
 
     public function index()
--- application/views/inicio.php
+++ application/views/inicio.php
@@ -7,7 +7,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	</head>
 	<body>
-		<div class="container">
+	<div class="container">
 			<!-- Título de oa página -->
 			<div class="row">
 				<h2>CRUD EN CI 3</h2>
